b3f5bd49266988a22f571da68925461e51bd4b32
Cynthia Salinas <csalinas123@gmail.com>
2015-11-21 07:51:53 -0800

    create and add data to collection

--- resolutions/resolutions.js
+++ resolutions/resolutions.js
@@ -1,5 +1,16 @@
-if (Meteor.isClient) {
+// creating "Resolutions" collection 
+Resolutions = new Mongo.Collection('resolutions'); 
 
+// Display database records into template 
+if (Meteor.isClient) {
+  // creating template helper - uses information from the html body
+  Template.body.helpers({
+    // calling the resolutions items from the collection 
+      resolutions: function() {
+        // finding all resolutions from the collection and searching the collection to find the varies records
+        return Resolutions.find(); 
+      }
+  });  
 }
 
 if (Meteor.isServer) {
