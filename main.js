9e0302b94a8c30a548f940f87bb716629975f89b
vframbach <vframbach@gmail.com>
2015-11-21 22:24:36 -0800

    changed colors, type of light, box

--- main.js
+++ main.js
@@ -1,161 +1,141 @@
-$(document).ready(function() {
+// Three.js ray.intersects with offset canvas
+
+var container, camera, scene, renderer, mesh,
+
+    objects = [],
+    
+    count = 0,
+
+    CANVAS_WIDTH = 400,
+    CANVAS_HEIGHT = 400;
+
+container = document.getElementById( 'canvas' );
+document.body.appendChild( container );
+
+renderer = new THREE.WebGLRenderer();
+renderer.setSize( CANVAS_WIDTH, CANVAS_HEIGHT );
+container.appendChild( renderer.domElement );
+
+scene = new THREE.Scene();
+
+camera = new THREE.PerspectiveCamera( 45, CANVAS_WIDTH / CANVAS_HEIGHT, 1, 1000 );
+camera.position.x = 600;
+camera.position.y = 100;
+camera.position.z = 600;
+camera.lookAt( {x: 0, y: 100, z: 0} );
+scene.add( camera );
+
+scene.add( new THREE.AmbientLight( 0x222222 ) );
+
+var lampHeight = 300;
+
+var spotLight = new THREE.SpotLight( 0xffffff );
+spotLight.position.set( 100, 300, 100 );
+scene.add( spotLight );
 
 
+//var pointLightRight = new THREE.PointLight( 0xffffff, 1 );
+    //pointLightRight.position.x = 50;
+    //pointLightRight.position.y = lampHeight;
+    //pointLightRight.position.z = 0;
+//scene.add( pointLightRight );
 
-    var container, camera, scene, renderer, mesh,
-
-        objects = [],
-
-        count = 0,
-
-        CANVAS_WIDTH = 400,
-        CANVAS_HEIGHT = 400;
-
-
-    container = document.getElementById('canvas');
-    // document.body.appendChild(container);
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
-    container.appendChild(renderer.domElement);
-
-    scene = new THREE.Scene();
-
-    camera = new THREE.PerspectiveCamera(45, CANVAS_WIDTH / CANVAS_HEIGHT, 1, 1000);
-    camera.position.x = 600;
-    camera.position.y = 100;
-    camera.position.z = 600;
-    camera.lookAt({
-        x: 0,
-        y: 100,
-        z: 0
-    });
-    scene.add(camera);
-
-    scene.add(new THREE.AmbientLight(0x222222));
-
-    var lampHeight = 300;
-
-    var pointLightRight = new THREE.PointLight(0xffffff, 1);
-    pointLightRight.position.x = 50;
-    pointLightRight.position.y = lampHeight;
-    pointLightRight.position.z = 0;
-    scene.add(pointLightRight);
-
-    var pointLightLeft = new THREE.PointLight(0xffffff, 1);
-    pointLightLeft.position.x = -50;
-    pointLightLeft.position.y = lampHeight;
-    pointLightLeft.position.z = 0;
-    scene.add(pointLightLeft);
-
-    var lampshadeMaterial = new THREE.MeshPhongMaterial({
-        color: 0xFFD800
-    });
-    lampshadeMaterial.opacity = 0.6;
-    lampshadeMaterial.transparent = true;
-    lampshade = new THREE.Mesh(
-        new THREE.CylinderGeometry(70, 90, 125, 32, 1, true),
-        lampshadeMaterial
-    );
-
-    lampshade.side = THREE.DoubleSide;
-    lampshade.position.x = 0;
-    lampshade.position.y = lampHeight;
-    lampshade.position.z = 0;
-    scene.add(lampshade);
-    objects.push(lampshade);
+//var pointLightLeft = new THREE.PointLight( 0xffffff, 1 );
+    //pointLightLeft.position.x = -50;
+    //pointLightLeft.position.y = lampHeight;
+    //pointLightLeft.position.z = 0;
+//scene.add( pointLightLeft ); 
 
 
-    lamppost = new THREE.Mesh(
-        new THREE.CylinderGeometry(15, 15, lampHeight, 32, 1),
-        new THREE.MeshPhongMaterial({
-            color: 0x654321
-        }));
-    lamppost.position.x = 0;
-    lamppost.position.y = lampHeight / 2;
-    lamppost.position.z = 0;
-    scene.add(lamppost);
-    objects.push(lamppost);
+var lampshadeMaterial = new THREE.MeshPhongMaterial( { color : 0xFFD800 } ); 
+lampshadeMaterial.opacity = 0.6;
+lampshadeMaterial.transparent = true;
+lampshade = new THREE.Mesh( 
+    new THREE.CylinderGeometry( 70, 90, 125, 32, 1, true ),
+    lampshadeMaterial
+ );
 
-    base = new THREE.Mesh(
-        new THREE.CylinderGeometry(80, 80, 5, 32, 1),
-        new THREE.MeshPhongMaterial({
-            color: 0xE45537
-        }));
-    base.position.x = 0;
-    base.position.y = 8;
-    base.position.z = 0;
-    scene.add(base);
-    objects.push(base);
+lampshade.side = THREE.DoubleSide;
+lampshade.position.x = 0;
+lampshade.position.y = lampHeight;
+lampshade.position.z = 0;
+scene.add( lampshade );
+objects.push( lampshade );
 
-    tabletop = new THREE.Mesh(
-        new THREE.BoxGeometry(400, 15, 400),
-        new THREE.MeshBasicMaterial({
-            color: 0x4679BD
-        }));
-    tabletop.position.x = 0;
-    tabletop.position.y = 0;
-    tabletop.position.z = 0;
-    scene.add(tabletop);
-    objects.push(tabletop);
+lamppost = new THREE.Mesh(
+    new THREE.CylinderGeometry( 15, 15, lampHeight, 32, 1 ),
+    new THREE.MeshPhongMaterial( { color : 0x654321 }
+) );
+lamppost.position.x = 0;
+lamppost.position.y = lampHeight / 2;
+lamppost.position.z = 0;
+scene.add( lamppost );
+objects.push( lamppost );
 
-    // find intersections
-    var raycaster = new THREE.Raycaster();
-    var mouse = new THREE.Vector2();
+base = new THREE.Mesh(
+    new THREE.CylinderGeometry( 80, 80, 80, 32, 1 ),
+    new THREE.MeshPhongMaterial( { color : 0xE45537 }
+) );
+base.position.x = 0;
+base.position.y = 8;
+base.position.z = 0;
+scene.add( base );
+objects.push( base );
 
-    // mouse listener
-    document.addEventListener('mousedown', function(event) {
+tabletop = new THREE.Mesh(
+            new THREE.BoxGeometry( 600, 400, 600 ),
+            new THREE.MeshPhongMaterial( {color: 0x4679BD, side: THREE.DoubleSide}
+) );
+tabletop.position.x = 300;
+tabletop.position.y = 200;
+tabletop.position.z = 300;
+scene.add( tabletop );
+objects.push( tabletop );
 
-        // For the following method to work correctly, set the canvas position *static*; margin > 0 and padding > 0 are OK
-        mouse.x = ((event.clientX - renderer.domElement.offsetLeft) / renderer.domElement.width) * 2 - 1;
-        mouse.y = -((event.clientY - renderer.domElement.offsetTop) / renderer.domElement.height) * 2 + 1;
 
-        // For this alternate method, set the canvas position *fixed*; set top > 0, set left > 0; padding must be 0; margin > 0 is OK
-        //mouse.x = ( ( event.clientX - container.offsetLeft ) / container.clientWidth ) * 2 - 1;
-        //mouse.y = - ( ( event.clientY - container.offsetTop ) / container.clientHeight ) * 2 + 1;
+// find intersections
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
 
-        raycaster.setFromCamera(mouse, camera);
+// mouse listener
+document.addEventListener( 'mousedown', function( event ) {
+    
+    // For the following method to work correctly, set the canvas position *static*; margin > 0 and padding > 0 are OK
+    mouse.x = ( ( event.clientX - renderer.domElement.offsetLeft ) / renderer.domElement.width ) * 2 - 1;
+    mouse.y = - ( ( event.clientY - renderer.domElement.offsetTop ) / renderer.domElement.height ) * 2 + 1;
+    
+    // For this alternate method, set the canvas position *fixed*; set top > 0, set left > 0; padding must be 0; margin > 0 is OK
+    //mouse.x = ( ( event.clientX - container.offsetLeft ) / container.clientWidth ) * 2 - 1;
+    //mouse.y = - ( ( event.clientY - container.offsetTop ) / container.clientHeight ) * 2 + 1;
 
-        intersects = raycaster.intersectObjects(objects);
+    raycaster.setFromCamera( mouse, camera );
 
-        if (intersects.length > 0) {
+    intersects = raycaster.intersectObjects( objects );
 
-            if (pointLightLeft.intensity === 0) {
-                pointLightLeft.intensity = 1;
-                pointLightRight.intensity = 1;
+    if ( intersects.length > 0 ) {
+
+            if (spotLight.intensity === 0) {
+                spotLight.intensity = 1;
             } else {
-                pointLightLeft.intensity = 0;
-                pointLightRight.intensity = 0;
+                spotLight.intensity = 0;
             }
-
-        }
-
-    }, false);
-
-    function render() {
-
-
-        renderer.render(scene, camera);
-
+        
     }
 
-    (function animate() {
+}, false );
 
-        requestAnimationFrame(animate);
+function render() {
 
-        render();
+    
+    renderer.render( scene, camera );
 
-    })();
+}
 
-$('.on-off-button').on('click', function() {
-    if (pointLightLeft.intensity === 0 ) {
-        pointLightLeft.intensity = 1;
-        pointLightRight.intensity = 1;
-    } else {
-        pointLightLeft.intensity = 0;
-        pointLightRight.intensity = 0;
-    }
+(function animate() {
 
-});
+    requestAnimationFrame( animate );
 
-});
+    render();
+
+})();
+
