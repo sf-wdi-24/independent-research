11cc79c7577ba7d4a1d325e9c85d6f84e34c41a4
alanlee35 <alanblee12@gmail.com>
2015-11-22 22:44:29 -0800

    pong ball bounces on both x and y axis

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@ $(function() {
 
 	var canvasContext;
 	var canvas;
-	var ball = 10;
-	var ballSpeed = 20
+	//ball x and y axis
+	var ballX = 10;
+	var ballSpeedX = 10;
+	var ballY = 10;
+	var ballSpeedY = 4;
 
 	// canvas = document.getElementById('newCanvas');
 	canvas = $('#newCanvas')[0];
@@ -16,7 +19,7 @@ $(function() {
 	//use setInterval to move 
 	var fps = 30;
 	setInterval(function () {
-		move();
+		moveBall();
 		draw();
 	}, 1000/fps);
 
@@ -26,23 +29,45 @@ $(function() {
 
 
 
-function move() {
-	ball = ball + ballSpeed;
+function moveBall() {
+	//x and y coordiantes changes as set interval calls this function
+	ballX = ballX + ballSpeedX;
+	ballY = ballY + ballSpeedY;
+
 	//if ball width exceeds the width of the canvas
-	if(ball > (canvas.width - 10)) {
-		ballSpeed = -ballSpeed;
+	if(ballX > (canvas.width - 10)) {
+		ballSpeedX = -ballSpeedX;
 	}
 	//0 is the left side of the board width
-	if (ball < 0) {
-		ballSpeed = -ballSpeed;
+	if (ballX < 0) {
+		ballSpeedX = -ballSpeedX;
+	};
+	//if ballY height exceeds the height of the canvas
+	if(ballY > (canvas.height - 7)) {
+		ballSpeedY = -ballSpeedY;
+	}
+	//0 is the left side of the board width
+	if (ballY < 0) {
+		ballSpeedY = -ballSpeedY;
 	};
 }
 function draw() {
 	colorRect(0, 0, canvas.width, canvas.height, 'black');
 	colorRect(0, 40, 10, 90, 'white');
-	colorRect(ball, 40, 10, 10, 'red');
+	//draw circle
+	makeCircle(ballX, ballY, 7, 'yellow');
+	
 	};
 
+function makeCircle(centerX, centerY, radius, color) {
+	//build the circle
+	canvasContext.fillStyle = color;
+	canvasContext.beginPath();
+	// .arc parameters (x, y, radius, startAngle, endAngle, counterclockwise or clockwise fill)
+	//endAngle using radians because internet said so..math.pi
+	canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
+	canvasContext.fill();
+}
 function colorRect(leftX, topY, width, height, color) {
 	canvasContext.fillStyle = color;
 	canvasContext.fillRect(leftX, topY, width, height);
