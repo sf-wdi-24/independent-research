4db47ddcddf13200459d6b60f64135999a339176
alanlee35 <alanblee12@gmail.com>
2015-11-22 18:45:08 -0800

    going to play wkth html canvas instead of paper.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(function() {
 	console.log('loaded');
 
+	var canvasContext;
+	var canvas;
 
-	
+
+	// canvas = document.getElementById('newCanvas');
+	canvas = $('#newCanvas')[0];
+	canvasContext = canvas.getContext('2d');
+	canvasContext.fillStyle = 'black';
+	canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 })
