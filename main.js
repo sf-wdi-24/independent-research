9eebc57baf58a59d85911c5826eb610940ad83a0
stevennoble78 <stevennoble78@hotmail.com>
2015-11-23 00:42:57 -0800

    translation completed

--- main.js
+++ main.js
@@ -44,122 +44,16 @@ $(function() {
 			controls,
 			meshMaterial;
 
+	// Declare global variables for original and transformed triangles
 	var triangleABC = { ax: 0, ay: 0, bx: 0, by: 0, cx: 0, cy: 0};
 	var triangleDEF = { dx: 0, dy: 0, ex: 0, ey: 0, fx: 0, fy: 0};
-	var triABC = new THREE.Geometry();
-	var triDEF = new THREE.Geometry();
-
 
 	// Set event handlers
 	$coordinates.on('click', getPoints);
 	$translationBtn.on('click', translation);
 	$reflectionBtn.on('click', reflection);
 	$rotationBtn.on('click', rotation);
-	$translationSubmit.on('click', renderTranslation);
-
-	function translation() {
-		event.preventDefault();
-		$body.css('background-color', '#ffb2b2');
-		$box.css('background-color', '#ffcccc');
-		$chooseTransformation.toggle();
-		$transformationBtns.toggle();
-		$translation.toggle();
-	}
-
-	function renderTranslation() {
-		event.preventDefault();
-		var h = Number($horizontalSlide.val());
-		var k = Number($verticalSlide.val());
-		triangleDEF.dx = triangleABC.ax + h;
-		triangleDEF.dy = triangleABC.ay + k;
-		triangleDEF.ex = triangleABC.bx + h;
-		triangleDEF.ey = triangleABC.by + k;
-		triangleDEF.fx = triangleABC.cx + h;
-		triangleDEF.fy = triangleABC.cy + k;
-		console.log(triangleDEF);
-		var vertexD = new THREE.Vector3(triangleDEF.dx, triangleDEF.dy, 0);
-		var vertexE = new THREE.Vector3(triangleDEF.ex, triangleDEF.ey, 0);
-		var vertexF = new THREE.Vector3(triangleDEF.fx, triangleDEF.fy, 0);
-		triDEF.vertices.push(vertexD);
-		triDEF.vertices.push(vertexE);
-		triDEF.vertices.push(vertexF);
-		triDEF.faces.push( new THREE.Face3(0,1,2));
-		triDEF.computeFaceNormals();
-		var mesh2 = new THREE.Mesh( triDEF, new THREE.MeshNormalMaterial() );
-		scene.add(mesh2);
-		insertNewPoints();
-		$newTriangle.toggle();
-	}
-
-	function reflection() {
-		event.preventDefault();
-		$body.css('background-color', '#b2ffb2');
-		$box.css('background-color', '#ccffcc');
-		$chooseTransformation.toggle();
-		$transformationBtns.toggle();
-		$reflection.toggle();
-	}
-
-	function rotation() {
-		event.preventDefault();
-		$body.css('background-color', '#b2b2ff');
-		$box.css('background-color', '#ccccff');
-		$chooseTransformation.toggle();
-		$transformationBtns.toggle();
-		$rotation.toggle();
-	}
-
-	function getPoints() {
-		event.preventDefault();
-		triangleABC.ax = Number($ax.val());
-		triangleABC.ay = Number($ay.val());
-		triangleABC.bx = Number($bx.val());
-		triangleABC.by = Number($by.val());
-		triangleABC.cx = Number($cx.val());
-		triangleABC.cy = Number($cy.val());
-		console.log(triangleABC);
-		var vertexA = new THREE.Vector3(triangleABC.ax, triangleABC.ay, 0);
-		var vertexB = new THREE.Vector3(triangleABC.bx, triangleABC.by, 0);
-		var vertexC = new THREE.Vector3(triangleABC.cx, triangleABC.cy, 0);
-		triABC.vertices.push(vertexA);
-		triABC.vertices.push(vertexB);
-		triABC.vertices.push(vertexC);
-		triABC.faces.push( new THREE.Face3(0,1,2));
-		triABC.computeFaceNormals();
-		var mesh = new THREE.Mesh( triABC, new THREE.MeshNormalMaterial() );
-		scene.add(mesh);
-		emptyPointBoxes();
-		insertPoints();
-		$points.toggle();
-		$transformations.toggle();
-	}
-
-	function emptyPointBoxes() {
-		$ax.val('');
-		$ay.val('');
-		$bx.val('');
-		$by.val('');
-		$cx.val('');
-		$cy.val('');
-	}
-
-	function insertPoints() {
-		$coordinateax.text(triangleABC.ax);
-		$coordinateay.text(triangleABC.ay);
-		$coordinatebx.text(triangleABC.bx);
-		$coordinateby.text(triangleABC.by);
-		$coordinatecx.text(triangleABC.cx);
-		$coordinatecy.text(triangleABC.cy);
-	}
-
-	function insertNewPoints() {
-		$coordinatedx.text(triangleDEF.dx);
-		$coordinatedy.text(triangleDEF.dy);
-		$coordinateex.text(triangleDEF.ex);
-		$coordinateey.text(triangleDEF.ey);
-		$coordinatefx.text(triangleDEF.fx);
-		$coordinatefy.text(triangleDEF.fy);
-	}
+	$translationSubmit.on('click', computeTranslation);
 
 	// Store height and width and set the canvas	
 	var width = $canvas.width();
@@ -182,27 +76,27 @@ $(function() {
 	camera.position.set( 0, 0, 25 );
 	camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
 
-	// Controls to move the camera
+	// // Controls to move the camera (Disabled)
 	// controls = new THREE.TrackballControls( camera );
 	// controls.rotateSpeed = 1.0;
 	// controls.zoomSpeed = 0.2;
 	// controls.panSpeed = 0.8;
-
 	// controls.noZoom = false;
 	// controls.noPan = false;
-
 	// controls.staticMoving = true;
 	// controls.dynamicDampingFactor = 0.3;
 
 	// Animate
 	animate();
 
+	// Animates the scene
 	function animate() {
 		requestAnimationFrame( animate );
 		// controls.update();
 		renderer.render( scene, camera );
 	}
 
+	// Function to build up the coordinate axes
 	function buildAxes( length ) {
 		var axes = new THREE.Object3D(),
 		index;
@@ -227,6 +121,7 @@ $(function() {
 		return axes;
 	}
 
+	// Function to build single line
 	function buildAxis( src, dst, colorHex, linewidth ) {
 		var geom = new THREE.Geometry(),
 			mat; 
@@ -235,6 +130,141 @@ $(function() {
 		geom.vertices.push( dst.clone() );
 		var axis = new THREE.Line( geom, mat, THREE.LineSegments );
 		return axis;
+	}
+
+	// On click, changes the background and toggles the translations window
+	function translation() {
+		event.preventDefault();
+		$body.css('background-color', '#ffb2b2');
+		$box.css('background-color', '#ffcccc');
+		$chooseTransformation.toggle();
+		$transformationBtns.toggle();
+		$translation.toggle();
+	}
+
+	// Takes the values for h and k and computes the points D, E, and F.
+	// Then calls function to render triangle DEF and show the new coordinates.
+	function computeTranslation() {
+		event.preventDefault();
+		var h = Number($horizontalSlide.val());
+		var k = Number($verticalSlide.val());
+		triangleDEF.dx = triangleABC.ax + h;
+		triangleDEF.dy = triangleABC.ay + k;
+		triangleDEF.ex = triangleABC.bx + h;
+		triangleDEF.ey = triangleABC.by + k;
+		triangleDEF.fx = triangleABC.cx + h;
+		triangleDEF.fy = triangleABC.cy + k;
+		console.log(triangleDEF);
+		renderTriangleDEF();
+		insertNewPoints();
+		$newTriangle.toggle();
+	}
+
+	// On click, changes the background and toggles the reflections window
+	function reflection() {
+		event.preventDefault();
+		$body.css('background-color', '#b2ffb2');
+		$box.css('background-color', '#ccffcc');
+		$chooseTransformation.toggle();
+		$transformationBtns.toggle();
+		$reflection.toggle();
+	}
+
+	// On click, changes the background and toggles the rotations window
+	function rotation() {
+		event.preventDefault();
+		$body.css('background-color', '#b2b2ff');
+		$box.css('background-color', '#ccccff');
+		$chooseTransformation.toggle();
+		$transformationBtns.toggle();
+		$rotation.toggle();
+	}
+
+	// Takes in the values of the points and calls functions to render
+	// the triangle and append the values to the page.  Then opens the
+	// transformations window
+	function getPoints() {
+		event.preventDefault();
+		triangleABC.ax = Number($ax.val());
+		triangleABC.ay = Number($ay.val());
+		triangleABC.bx = Number($bx.val());
+		triangleABC.by = Number($by.val());
+		triangleABC.cx = Number($cx.val());
+		triangleABC.cy = Number($cy.val());
+		console.log(triangleABC);
+		renderTriangleABC();
+		emptyPointBoxes();
+		insertPoints();
+		$points.toggle();
+		$transformations.toggle();
+	}
+ 
+ 	// Adds triangle ABC to the grid
+	function renderTriangleABC() {
+		var triABC = new THREE.Geometry();
+		// Define the vertices
+		var vertexA = new THREE.Vector3(triangleABC.ax, triangleABC.ay, 0);
+		var vertexB = new THREE.Vector3(triangleABC.bx, triangleABC.by, 0);
+		var vertexC = new THREE.Vector3(triangleABC.cx, triangleABC.cy, 0);
+		// Push the vertices to the triangle
+		triABC.vertices.push(vertexA);
+		triABC.vertices.push(vertexB);
+		triABC.vertices.push(vertexC);
+		// Create the face
+		triABC.faces.push( new THREE.Face3(0,1,2));
+		triABC.computeFaceNormals();
+		// Give the triangle a mesh and add it to the scene
+		var mesh = new THREE.Mesh( triABC, new THREE.MeshNormalMaterial() );
+		scene.add(mesh);
+	}
+
+	// Adds triangle DEF to the grid
+	function renderTriangleDEF() {
+		var triDEF = new THREE.Geometry();
+		// Define the vertices
+		var vertexD = new THREE.Vector3(triangleDEF.dx, triangleDEF.dy, 0);
+		var vertexE = new THREE.Vector3(triangleDEF.ex, triangleDEF.ey, 0);
+		var vertexF = new THREE.Vector3(triangleDEF.fx, triangleDEF.fy, 0);
+		// Push the vertices to the triangle
+		triDEF.vertices.push(vertexD);
+		triDEF.vertices.push(vertexE);
+		triDEF.vertices.push(vertexF);
+		// Create the face
+		triDEF.faces.push( new THREE.Face3(0,1,2));
+		triDEF.computeFaceNormals();
+		// Give the triangle a mesh and add it to the scene
+		var mesh2 = new THREE.Mesh( triDEF, new THREE.MeshNormalMaterial() );
+		scene.add(mesh2);
+	}
+
+	// Clears the coordinates out of the boxes to be reused
+	function emptyPointBoxes() {
+		$ax.val('');
+		$ay.val('');
+		$bx.val('');
+		$by.val('');
+		$cx.val('');
+		$cy.val('');
+	}
+
+	// Appends the points for triangle ABC to the page
+	function insertPoints() {
+		$coordinateax.text(triangleABC.ax);
+		$coordinateay.text(triangleABC.ay);
+		$coordinatebx.text(triangleABC.bx);
+		$coordinateby.text(triangleABC.by);
+		$coordinatecx.text(triangleABC.cx);
+		$coordinatecy.text(triangleABC.cy);
+	}
+
+	// Appends the points for triangle DEF to the page
+	function insertNewPoints() {
+		$coordinatedx.text(triangleDEF.dx);
+		$coordinatedy.text(triangleDEF.dy);
+		$coordinateex.text(triangleDEF.ex);
+		$coordinateey.text(triangleDEF.ey);
+		$coordinatefx.text(triangleDEF.fx);
+		$coordinatefy.text(triangleDEF.fy);
 	}
 
 });
