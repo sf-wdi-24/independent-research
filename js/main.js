f8bb9f396852683eab843ca185837142b359ba32
alanlee35 <alanblee12@gmail.com>
2015-11-22 19:35:32 -0800

    game board set up and moving function in the works

--- js/main.js
+++ js/main.js
@@ -3,11 +3,42 @@ $(function() {
 
 	var canvasContext;
 	var canvas;
-
+	var ball = 50;
 
 	// canvas = document.getElementById('newCanvas');
 	canvas = $('#newCanvas')[0];
 	canvasContext = canvas.getContext('2d');
+	//game board
+	// canvasContext.fillStyle = 'black';
+	// canvasContext.fillRect(0,0,canvas.width, canvas.height);
+
+	//use setInterval to move 
+	var fps = 30;
+	setInterval(function () {
+		move();
+		draw();
+	}, 1000/fps);
+
+
+
+
+
+
+
+function move() {
+	ball = ball + 5
+}
+function draw() {
 	canvasContext.fillStyle = 'black';
-	canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-})
+	canvasContext.fillRect(0,0,canvas.width, canvas.height);
+	canvasContext.fillStyle = 'white';
+	canvasContext.fillRect(0, 40, 10, 90);
+	canvasContext.fillStyle = 'red';
+	canvasContext.fillRect(ball, 40, 10, 10);
+	};
+
+
+
+
+
+});
